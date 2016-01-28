db468f751b47fd626af6775db27b70cc444f8fd6
Egemen Can Uze <egemencanuze@gmail.com>
2016-01-28 18:23:12 +0200

    Add search button and slide menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,5 +46,11 @@ $(function() {
     pause: false  // do not pause on hover
   });
 
-
+  var sideslider = $('[data-toggle=collapse-side]');
+  var sel = sideslider.attr('data-target');
+  var sel2 = sideslider.attr('data-target-2');
+  sideslider.click(function(event){
+      $(sel).toggleClass('in');
+      $(sel2).toggleClass('out');
+  });
 });

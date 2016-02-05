2150ecda4076278a5a0b330009b7d0c9947d8a64
Egemen Can Uze <egemencanuze@gmail.com>
2016-02-05 11:39:02 +0200

    Add omniauth-facebook

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'devise'
+gem 'omniauth'
+gem 'omniauth-facebook'
 
 group :development, :test do
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   root 'welcome#home' 
+  
+  devise_for :users,
+             path: '',
+             path_names: { sign_in: 'login', sign_out: 'logout', edit: 'profile' },
+             controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

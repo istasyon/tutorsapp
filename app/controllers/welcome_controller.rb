ef32c8ee9c2f5e737787f80250827c6cae7f3443
Egemen Can Uze <egemencanuze@gmail.com>
2016-02-17 11:55:00 +0200

    Fix become a tutor link

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,6 @@ class WelcomeController < ApplicationController
   def home
   end
   def become_a_teacher
-  	if !user_signed_in?
-		redirect_to new_user_session_path
-    else
-    	render "become_a_teacher"
-    end
   end
   def search_results
   end

67c81bc99732a641680ad80990592a0a3cdd1095
Egemen Can Uze <egemencanuze@gmail.com>
2016-02-29 11:25:43 +0200

    Add options for select to search form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ApplicationController
   before_action :authenticate_user!, only: :become_a_teacher
 
   def home
+  	@languages = Language.all
+  	@locations = User.uniq.pluck(:location)
   end
   def become_a_teacher
   	@user = current_user

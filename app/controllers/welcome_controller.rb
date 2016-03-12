0209acce9c15577436cd827b9e199dfa4d910433
Egemen Can Uze <egemencanuze@gmail.com>
2016-03-12 12:45:33 +0200

    Redirect current page after oauth sign in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,10 @@
 class WelcomeController < ApplicationController
-  
-  before_action :authenticate_user!, only: :become_a_teacher
 
   def home
   	@languages = Language.all
   	@locations = Teacher.uniq.pluck(:location)
   end
+  
   def become_a_teacher
   	@user = current_user
   end

89ee1fc1d5568b0ab535c899ad4fc2c747236f46
Egemen Can Uze <egemencanuze@gmail.com>
2016-03-15 18:54:33 +0200

    Solved dashboard N+1 problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def dashboard
     @user = current_user
-    @student_appointments = @user.appointments
-    @teacher_appointments = Appointment.where(listing_id: Listing.where(user_id: current_user.id))
-    @listings = @user.listings
+    @student_appointments = @user.appointments.includes(:listing)
+    @teacher_appointments = Appointment.where(listing_id: Listing.where(user_id: current_user.id)).includes(:user)
+    @listings = @user.listings.includes(:language)
     @my_students = User.where(id: @teacher_appointments.select(:user_id).distinct)
     @my_teachers = User.where(id: Listing.where(id: @student_appointments.select(:listing_id).distinct).select(:user_id).distinct)
 
